b86504536d6deb6140f0d8c1c448239e61d2d607
CodingWithKarim <karimhassan92@gmail.com>
2025-02-04 06:38:12 -0500

    clean-up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { ToggleThemeButton } from "./ToggleThemeButton";
 export const Header: React.FC = () => {
   return (
     <header className="header">
-      <h1>My Portfolio</h1>
+      <h1>Karim's Portfolio</h1>
       <ToggleThemeButton />
     </header>
   );
